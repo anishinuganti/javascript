160ee020f0bb875a8e748b804abe27901f1851b0
Anish Inuganti <anish.inuganti@thebluespire.com>
2026-04-06 13:15:16 +0530

    variables&types

---
+++ variablestypes.js
@@ -0,0 +1,36 @@
+//task 1
+
+let name = "anish";
+let age = 22;
+let city = "hyderabad";
+
+let res = `Hi my name is ${name}, I am ${age} years old, and I live in ${city}.`;
+
+console.log(res);
+
+//task 2
+
+console.log("5" + 2); 
+// Explanation: Number 2 is converted to string so it becomes "5" + "2"
+
+console.log("5" - 2); 
+// Explanation: string "5" becomes 5->5-2->3
+
+
+console.log(false + "5"); 
+// Explanation: false becomes string "false" and will be concatenated with 5
+
+
+//task 3
+
+let str = "Hello";          
+let num = 10;              
+let isboolean = true;       
+let value = null;          
+let k;            
+
+console.log(str, typeof str);
+console.log(num, typeof num);
+console.log(isboolean, typeof isboolean);
+console.log(value, typeof value);     
+console.log(k, typeof k);
